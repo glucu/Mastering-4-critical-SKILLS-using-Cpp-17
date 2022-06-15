2c80d21b53fdb079f466b63f1dd2a8dbaefdd668
Gabriel Luculescu <gabe.luculescu@gmail.com>
2022-06-15 14:31:50 -0500

    Update solution for homework-6.cpp

--- section-7/for-loops/homework-6.cpp
+++ section-7/for-loops/homework-6.cpp
@@ -2,6 +2,22 @@
 
 int main() {
 
+    int n{}, a{}, b{}, sum{};
+    std::cin >> n >> a >> b;
+
+    for(int i{1}; i <= n; ++i) {
+        int tmp{i};
+        int digitsSum{};
+
+        while(tmp > 0) {
+            digitsSum += tmp % 10;
+            tmp /= 10;
+        }
+        if(a <= digitsSum && digitsSum <= b) {
+            sum += i;
+        }
+    }
+    std::cout << sum << '\n';
     
     return 0;
 }
