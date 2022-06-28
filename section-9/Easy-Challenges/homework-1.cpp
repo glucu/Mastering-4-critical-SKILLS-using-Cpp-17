504fc9620a39071f49ff0b186d82da65e399b8ae
Gabriel Luculescu <gabe.luculescu@gmail.com>
2022-06-28 14:30:02 -0500

    Update homework-1.cpp

--- section-9/Easy-Challenges/homework-1.cpp
+++ section-9/Easy-Challenges/homework-1.cpp
@@ -3,19 +3,29 @@
 
 int main() {
 
-	std::string s, t;
-	std::cin >> s >> t;
+	std::string input, str;
+	std::cin >> input >> str;
 
-	std::size_t maxLength{ s.size() > t.size() ? s.size() : t.size() };
-	for(std::size_t i{}; i != maxLength; ++i) {
-		if(i != s.size()) {
-			std::cout << s[i];
-		}
-		if(i != t.size()) {
-			std::cout << t[i];
-		}
+	if(input.size() < str.size()) {
+		std::cout << "NO\n";
 	}
-	std::cout << '\n';
+	else {
+		bool isPrefix{true};
+		for(std::size_t i{}; i != str.size(); ++i) {
+			if(str[i] != input[i]) {
+				isPrefix = false;
+				break;
+			}
+		}
+
+		if(isPrefix) {
+			std::cout << "YES\n";
+		}
+		else {
+			std::cout << "NO\n";
+		}
+		
+	}
 
 	return 0;
 }
